52dd842034be0a2f1e61b7cc87ebf2b00785db38
mrzhou <463913540@qq.com>
2020-03-08 17:55:09 +0800

    编写tabbar导航条

---
+++ src/components/tabbar.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="tabbar">
+		<div class="left">
+			<li>松果出行</li>
+			<li>MIMU</li>
+			<li>云服务</li>
+		</div>
+		<div class="right">
+			<li>登陆</li>
+			<li>注册</li>
+			<li class="add-shopping-btn">添加购物车</li>
+		</div>
+		
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Tabbar'
+	}
+</script>
+
+<style scoped="scoped">
+		width: 100%;
+		background: rgba(51,51,51,1);
+		margin: auto;
+		display: flex;
+		justify-content: space-between;
+		align-content: center;
+		padding: 0 22px;
+	}
+	.left,.right{
+		display: flex;
+		/* padding: 10px; */
+	}
+	.left li,.right li{
+		font-size: 12px;
+		margin: 0 10px;
+		color: #B0B0B0;
+		line-height: 30px;
+	}
+	.left li:hover,.right li:hover{
+		color: aquamarine;
+		cursor: pointer;
+	}
+	.add-shopping-btn{
+		background: rgba(102,102,102,1);
+		display: block;
+		height: 30px;
+		width: 110px;
+		text-align: center;
+		line-height: 30px;
+	}
+	
+</style>
